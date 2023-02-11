d624ba20e17c9fbd4f985606b6379096545c3d68
unknown <shady.samara@gmail.com>
2023-02-11 01:14:08 -0800

    lecture2

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -2,6 +2,7 @@ import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 import 'package:test/scaffolding.dart';
 
+enum Certification { diploma, ba, master, phd }
 void main(List<String> arguments) {
   num number1 = 1;
   int number2 = 5;
@@ -40,14 +41,38 @@ void main(List<String> arguments) {
   //   return element.length;
   // });
   // List<int> lengths = namesLengths.toList();
-  print(names1);
-  names1.sort((name1, name2) {
-    return name2.length.compareTo(name1.length);
-  });
-  print(names1);
-// [omar, shaker, majd, ahmed, reem]
-// [omar, majd, reem, ahmed, shaker]
+  // print(names1);
+  // names1.sort((name1, name2) {
+  //   return name2.length.compareTo(name1.length);
+  // });
+  // print(names1);
+  List names = ["omar", "amal", "omar", "farah"];
+  // Set namesSet = {"omar", "amal", "omar", "farah"};
+  // dynamic myName = "shady";
+  // myName = 9;
 
-// [omar, shaker, majd, ahmed, reem]
-// [shaker, ahmed, omar, majd, reem]
+  Map<String, dynamic> data = {
+    "name": "ahmed",
+    "name": "shaker",
+    "age": 12,
+    "isMale": true
+  };
+  // data.forEach((key, value) {
+  //   print('the key is $key, and the value is $value');
+  // });
+  // Set<String> dataKeys = data.keys.toSet();
+  // dataKeys.forEach((element) {
+  //   print(data[element]);
+  // });
+  // Certification ahmedCertification = Certification.master;
+// function signeture // function header
+// return type , function name , parameters
+// function body
+// positional parameters
+// optional positional parameters
+// optional named parameters
+  int sum(int num1,{ int num2=0}) => num1 + num2;
+
+
+  print(sum(9));
 }
